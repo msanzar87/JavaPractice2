53ec5218ca9147e745d12c460ff23009a121f503
munir sanzar <msanzar87@gmail.com>
2021-09-27 19:26:27 -0400

    IfElse added

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int grade = 45;
+
+        if (grade > 50){
+            System.out.println("you passed!");
+        }else{
+            System.out.println("Try again please!");
+        }
+
+    }
+}
