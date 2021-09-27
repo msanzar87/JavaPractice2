e7c729110286840d4078878b3cae75a98104c2bd
munir sanzar <msanzar87@gmail.com>
2021-09-27 19:32:43 -0400

    IfElse added conditions

--- IfElse.java
+++ IfElse.java
@@ -1,8 +1,8 @@
 public class IfElse {
     public static void main(String[] args) {
-        int grade = 45;
+        int grade = 50;
 
-        if (grade > 50){
+        if (grade >= 50){
             System.out.println("you passed!");
         }else{
             System.out.println("Try again please!");
