4280687636ef3dd690af12071ee53eae7bff75be
munir sanzar <msanzar87@gmail.com>
2021-09-29 00:10:40 -0400

    Delimiters made

---
+++ Delimiters.java
@@ -0,0 +1,9 @@
+public class Delimiters {
+    public static void main(String[] args) {
+        System.out.println("Enter two integers (on the same line)");
+        System.out.println("Enter two very integers (on the same line)");
+        System.out.println("Enter two decimals (on the same line)");
+        System.out.println("Enter two text values (on the same line)");
+
+    }
+}
