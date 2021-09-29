395b03e44e61bb99bda8de91b2955374010314f2
munir sanzar <msanzar87@gmail.com>
2021-09-29 05:00:25 -0400

    Functions lesson 1

---
+++ Chorus.java
@@ -0,0 +1,8 @@
+public class Chorus {
+    public static void main(String[] args) {
+        System.out.println("Dont blame it on the sunshine.");
+        System.out.println("Dont blame it on the moonlight.");
+        System.out.println("Dont blame it on the good times.");
+        System.out.println("Blame it on the boogie.\n");
+    }
+}
