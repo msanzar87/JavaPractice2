6bf7de0b046c633541bd2ac9866424aaa45499af
munir sanzar <msanzar87@gmail.com>
2021-09-29 00:28:17 -0400

    Delimiters lesson 3

--- Delimiters.java
+++ Delimiters.java
@@ -4,28 +4,37 @@ public class Delimiters {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter two integers (on the same line)");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        System.out.println("Please enter a number");
+        int num = sc.nextInt();
 
-        System.out.println("Enter two very integers (on the same line)");
-        long bignum1 = sc.nextLong();
-        long bignum2 = sc.nextLong();
-
-        System.out.println("Enter two decimals (on the same line)");
-        double decimal1 = sc.nextDouble();
-        double decimal2 = sc.nextDouble();
-
-        System.out.println("Enter two text values (on the same line)");
-        String text1 = sc.next();
-        String text2 = sc.next();
+        sc.nextLine();
+        System.out.println("Please write a sentence");
+        String sentence = sc.nextLine();
 
         sc.close();
 
-        System.out.println("\tIntegers are:" + num1 + " " + num2);
-        System.out.println("\tBigIntegers are:" + bignum1 + " " + bignum2);
-        System.out.println("\tDecimals are:" + decimal1 + " " + decimal2);
-        System.out.println("\tText values are:" + text1 + " " + text2);
+//        System.out.println("Enter two integers (on the same line)");
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//
+//        System.out.println("Enter two very integers (on the same line)");
+//        long bignum1 = sc.nextLong();
+//        long bignum2 = sc.nextLong();
+//
+//        System.out.println("Enter two decimals (on the same line)");
+//        double decimal1 = sc.nextDouble();
+//        double decimal2 = sc.nextDouble();
+//
+//        System.out.println("Enter two text values (on the same line)");
+//        String text1 = sc.next();
+//        String text2 = sc.next();
+//
+//        sc.close();
+//
+//        System.out.println("\tIntegers are:" + num1 + " " + num2);
+//        System.out.println("\tBigIntegers are:" + bignum1 + " " + bignum2);
+//        System.out.println("\tDecimals are:" + decimal1 + " " + decimal2);
+//        System.out.println("\tText values are:" + text1 + " " + text2);
 
 
 
