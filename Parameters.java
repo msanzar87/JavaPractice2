1b0128b8536c288de596fd807d1bf6342c0d2d7d
munir sanzar <msanzar87@gmail.com>
2021-09-29 05:28:04 -0400

    Functions lesson 4

--- Parameters.java
+++ Parameters.java
@@ -5,7 +5,8 @@ public class Parameters {
 
     public static void measureRectangle(double length, double width){
         double area = length * width;
-        System.out.println("The area of the rectangle is " +area+ "\n");
+        System.out.println("The area of the rectangle with length " +length+ " and width "+width+
+                " is " +area+ "\n");
     }
 }
 
