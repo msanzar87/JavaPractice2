93c5526b73c01475f10af51b0f82141a18bb0db1
munir sanzar <msanzar87@gmail.com>
2021-10-01 05:30:55 -0400

    DiceGame 2nd part

--- DiceJack.java
+++ DiceJack.java
@@ -1,11 +1,38 @@
+import java.util.Scanner;
+
 public class DiceJack {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
     int roll1 = rollDice();
     int roll2 = rollDice();
     int roll3 = rollDice();
-        System.out.println("The first roll was: "+roll1);
-        System.out.println("The second roll was: "+roll2);
-        System.out.println("The third roll was: "+roll3);
+
+        System.out.println("Enter three numbers between 1 and 6");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        if (num1 < 1 || num2 < 1 || num3 < 1){
+            System.out.println("Numbers cannot be less than 1. Shutting game down!");
+            System.exit(0);
+        }
+        if (num1 > 6 || num2 > 6 || num3 > 6){
+            System.out.println("Numbers cannot be greater than 6. Shutting game down!");
+            System.exit(0);
+        }
+
+        int sumOfNumbers = num1+num2+num3;
+        int sumDiceRolls = roll1+roll2+roll3;
+        System.out.println("The number of dice rolls were: "+sumDiceRolls+" The sum of numbers were: "+sumOfNumbers);
+
+        if (checkWin(sumDiceRolls,sumOfNumbers)){
+            System.out.println("Congrats! You Win!");
+        }else {
+            System.out.println("You lost. Try again!");
+        }
+
+        sc.close();
 
     }
 
@@ -13,5 +40,11 @@ public class DiceJack {
         double randomNumber = Math.random() * 6;
         randomNumber +=1;
         return (int)randomNumber;
+    }
+
+    public static boolean checkWin(int sumDiceRolls, int sumOfNumbers){
+        return  (sumOfNumbers > sumDiceRolls && sumOfNumbers - sumDiceRolls < 3);
+
+
     }
 }
